e676d33375e3d9bc37cd4f8c45c429ea11bb075a
Nam Dao <nam.dao@eucalyptus.vc>
2021-12-25 19:14:05 +1000

    added boilerplate for react, added tailwindcss, attempted to add eslint

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+export const Home = (): ReactElement => {
+  return (
+    <div className="flex justify-center items-center">
+      <p>
+        This is the home page! Welcome aboard soldier :-)
+      </p>
+    </div>
+  )
+}
