e2222a82039a830b88e18497f39c477fdab75d4e
Nam Dao <nam.dao@eucalyptus.vc>
2021-12-27 17:58:23 +1000

    feat: added more structures to the login page and form hooks

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,45 @@
 import { ReactElement } from 'react';
 
 const Login = (): ReactElement => {
-  return <div>This is a login page!</div>;
+  const handleSubmit = () => {};
+
+  return (
+    <div className="flex justify-center">
+      <form className="p-6 w-96" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <div className="mb-4">
+            <div className="mb-2">
+              <label>Email</label>
+            </div>
+            <input
+              className="appearance-none border-2 rounded-lg w-full p-3 text-black focus:outline-none"
+              required
+              autoFocus
+              type="email"
+              name="email"
+            />
+          </div>
+          <div className="mb-4">
+            <div className="mb-2">
+              <label>Password</label>
+            </div>
+            <input
+              className="appearance-none border-2 rounded-lg w-full p-3 text-black focus:outline-none"
+              required
+              type="password"
+              name="password"
+            />
+          </div>
+        </div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
---
+++ frontend/src/services/auth-provider.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from 'react';
+
+export const AuthProvider = (): ReactElement => {
+  return <>Auth Provider</>;
+};
