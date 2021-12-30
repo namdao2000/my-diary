e95afbee2456ca5e90b70f840720ef9487fe2af5
Nam Dao <nam.dao@eucalyptus.vc>
2021-12-30 19:42:25 +1000

    fix: formatted the sql statements and added new routes to diary

--- backend/src/data-layer/sql-statements.ts
+++ backend/src/data-layer/sql-statements.ts
@@ -7,34 +7,40 @@ export const SQL_STATEMENTS = {
    *   Therefore, I have used the most recent 'completed_at' instead of most recent 'updated_at' records.
    */
   initialiseDatabase: `
-CREATE TABLE IF NOT EXISTS user (
-    username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    ip_address VARCHAR(255),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at DATETIME
-   );
+      CREATE TABLE IF NOT EXISTS user
+      (
+          username   VARCHAR(255) PRIMARY KEY,
+          password   VARCHAR(255) NOT NULL,
+          first_name VARCHAR(255) NOT NULL,
+          last_name  VARCHAR(255) NOT NULL,
+          ip_address VARCHAR(255),
+          created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+          updated_at DATETIME
+      );
 
-CREATE TABLE IF NOT EXISTS diary_document (
-    document_id UUID PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    content TEXT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at DATETIME,
-    FOREIGN KEY (username) REFERENCES user (username)
-    );
-`,
+      CREATE TABLE IF NOT EXISTS diary_document
+      (
+          document_id UUID PRIMARY KEY,
+          username    VARCHAR(255) NOT NULL,
+          content     TEXT,
+          created_at  DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+          updated_at  DATETIME,
+          FOREIGN KEY (username) REFERENCES user (username)
+      );
+  `,
   createNewUser: `
-  INSERT INTO user (username, password, first_name, last_name, ip_address)
-  VALUES (?, ?, ?, ?, ?)
+      INSERT INTO user (username, password, first_name, last_name, ip_address)
+      VALUES (?, ?, ?, ?, ?)
   `,
 
   getUserPassword: `
-  SELECT password from user where username = ? 
+      SELECT password
+      from user
+      where username = ?
   `,
   getUser: `
-  SELECT * from user where username = ?
+      SELECT *
+      from user
+      where username = ?
   `,
 };
--- backend/src/routes/diary.routes.ts
+++ backend/src/routes/diary.routes.ts
@@ -4,6 +4,7 @@ import { authTokenGuard } from '../middleware/token-verficiation.middleware';
 
 export const DiaryRouter = Router();
 DiaryRouter.get('/diary', authTokenGuard, DiaryController.get);
+DiaryRouter.get('/diary/:id', authTokenGuard, DiaryController.get);
 DiaryRouter.post('/diary', authTokenGuard, DiaryController.create);
-DiaryRouter.put('/diary', authTokenGuard, DiaryController.update);
-DiaryRouter.delete('/diary', authTokenGuard, DiaryController.delete);
+DiaryRouter.put('/diary/:id', authTokenGuard, DiaryController.update);
+DiaryRouter.delete('/diary/:id', authTokenGuard, DiaryController.delete);
